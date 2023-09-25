6c9f593f467da1a89228997611d600a618462c40
Juan Diego Lora <juan.lora1@u.icesi.edu.co>
2023-09-25 07:56:46 -0500

    View setted

--- app_becas/views/calendar.py
+++ app_becas/views/calendar.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.contrib.auth import logout
 
 
 @method_decorator(login_required, name='dispatch')
