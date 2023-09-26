bc6b1602274096506f20f56d4d01f1146eeddf77
Juan Diego Lora <juan.lora1@u.icesi.edu.co>
2023-09-25 22:51:36 -0500

    feat: Basic Django form added

--- app_becas/views/add_calendar.py
+++ app_becas/views/add_calendar.py
@@ -3,10 +3,14 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from app_becas.forms.add_calendar import CreateNewTaskForm
 
 @method_decorator(login_required, name='dispatch')
 class Add_calendar(View):
 
+
     def get(self, request):
-        return render(request, 'add_calendar.html')
+        return render(request, 'add_calendar.html',{
+            'form': CreateNewTaskForm()
+        })
     
