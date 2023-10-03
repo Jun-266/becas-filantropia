6ad2d22d0800202b70d451146d09bf30746209a4
santiagoprado21 <santiagoprado435@gmail.com>
2023-10-02 23:32:06 -0500

    feat(models): Agregar modelo Scholarship
    Se ha creado la clase Scholarship en el modelo de Django para representar las becas con los atributos name, description, amount y tipo_beca

--- app_becas/models.py
+++ app_becas/models.py
@@ -1,7 +1,21 @@
+import uuid
 from django.db import models
 
 class Scholarship(models.Model):
-    name = models.CharField
+    Type_Scholaship = [
+        ('Excelencia', 'Beca de Excelencia'),
+        ('Logros', 'Beca de Logros'),
+        ('Representantes', 'Beca de Representantes'),
+        ('Colaboradores', 'Beca de Colaboradores'),
+        ('Especial', 'Beca Especial'),
+        ('Familiar', 'Beca Familiar'),
+        ('Minorias', 'Beca de Minorías'),
+    ]
+    auto_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
     description = models.CharField(max_length=250)
     amount = models.IntegerField()
+    type = models.CharField(max_length=20, choices=Type_Scholaship)
 
+    def __str__(self):
+        return self.name
