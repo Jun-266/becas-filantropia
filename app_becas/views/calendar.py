97017849aeb828c2d0fbc42935ed40c186406c3c
Juan Diego Lora <juan.lora1@u.icesi.edu.co>
2023-10-05 15:19:14 -0500

    chore(calendar_*.html): it shows the complete calendar's info, change the stetic of the add calendar form, and add stetic to the search bar

--- app_becas/views/calendar.py
+++ app_becas/views/calendar.py
@@ -14,26 +14,28 @@ class Calendar(View):
     def get(self, request):
          # calendar = list(Calendar.objects.values())
         calendars = Calendar_obj.objects.all()
-        msg_any = ""
         return render(request, 'calendar.html', {
             'calendars': calendars,
-            'msg': ""
+            'msg': "",
+            'default_value_msg': "not-searched",
         }, )
     
     def post(self, request):
         
         calendars = Calendar_obj.objects.filter(scholarship_id = request.POST['to_search']) 
         
-        if calendars: 
+        if calendars: #Calendar not empty
             return render(request, 'calendar.html', {
                 'calendars': calendars,
-                'msg': ""
+                'msg': "",
+                'default_value_msg': calendars.__getitem__(0).scholarship_id,
             })
         else :
             msg_error = "No se encontró ninguna coincidencia"
             return render(request, 'calendar.html', {
                 'calendars': calendars, 
                 "msg": msg_error,
+                'default_value_msg': request.POST['to_search'],
             })
         
     def send_id(value):
