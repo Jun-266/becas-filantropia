6dc56675fd5deab6133b3833332fc6972e4ff069
Jeison Lasprilla <1114238132@u.icesi.edu.co>
2023-10-08 14:32:41 -0500

    feat(models.py): created models

--- app_becas/models.py
+++ app_becas/models.py
@@ -1,17 +1,6 @@
 import uuid
 from django.db import models
 
-class   Scholarship(models.Model):
-    temp = models.CharField(max_length=22)
-    auto_id = models.UUIDField(default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=250)
-    amount = models.IntegerField()
-    type_scholarship = models.CharField(max_length=50, choices=[('Excelencia', 'Excelencia'), ('Logros y Representantes', 'Logros y Representantes'), ('Colaboradores', 'Colaboradores'), ('Especial', 'Especial'), ('Familiar y Minorías', 'Familiar y Minorías')])
-
-    def __str__(self):
-        return self.name
-    
 class Calendar(models.Model):
         
     auto_id =  models.AutoField(primary_key = True)
@@ -25,6 +14,36 @@ class Calendar(models.Model):
     interview_deadline = models.DateField()
     publish_elected_start_date = models.DateField()
     publish_elected_deadline = models.DateField( )
+
+    def __str__(self):
+        return str(self.auto_id)
+    
+class Contact(models.Model):
+    auto_id = models.AutoField(primary_key = True)
+    identification = models.CharField(max_length=30)
+    type = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    phone = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.auto_id)
+    
+class Scholarship(models.Model):
+    temp = models.CharField(max_length=22)
+    auto_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=250)
+    amount = models.IntegerField()
+    type_scholarship = models.CharField(max_length=50, choices=[('Excelencia', 'Excelencia'), ('Logros y Representantes', 'Logros y Representantes'), ('Colaboradores', 'Colaboradores'), ('Especial', 'Especial'), ('Familiar y Minorías', 'Familiar y Minorías')])
+
+    def __str__(self):
+        return self.name
+    
+class Donor(models.Model):
+    scholarships = models.ManyToManyField(Scholarship, related_name='donors')
+    contacts = models.ManyToManyField(Contact, related_name='donors')
+    auto_id = models.AutoField(primary_key = True)
+    enterprise_name = models.CharField(max_length=20)
 
     def __str__(self):
         return str(self.auto_id)
