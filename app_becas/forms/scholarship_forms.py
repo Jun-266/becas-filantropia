42f8398937da4b02aaff51fa2b206f7133c3042b
santiagoprado21 <santiagoprado435@gmail.com>
2023-10-11 18:37:37 -0500

    fix(delete_type_scholarship): update models.py and Scholarship.Form

--- app_becas/forms/scholarship_forms.py
+++ app_becas/forms/scholarship_forms.py
@@ -26,15 +26,3 @@ class delete_type(forms.Form):
     delete_type_scholarship = forms.CharField(
         label= 'Nombre de tipo de beca:', 
     )
-
-class Form_add_calendar(forms.Form):
-    '''
-    inscription_start_date = forms.DateField(label= "Inicio de la Inscripción", widget=forms.SelectDateWidget)
-    inscription_deadline = forms.DateField(label= "Fin de la Inscripción", widget=forms.SelectDateWidget)
-    selection_start_date = forms.DateField(label= "Inicio de la Selección de participantes", widget=forms.SelectDateWidget)
-    selection_deadline = forms.DateField(label= "Fin de la Selección de participantes", widget=forms.SelectDateWidget)
-    interview_start_date = forms.DateField(label= "Inicio de Entrevistas a participantes", widget=forms.SelectDateWidget)
-    interview_deadline = forms.DateField(label= "Fin de la Entrevistas a participantes", widget=forms.SelectDateWidget)
-    publish_elected_start_date = forms.DateField(label= "Inicio de Publicación de resultados", widget=forms.SelectDateWidget)
-    publish_elected_deadline = forms.DateField(label= "Fin de Publicación de resultados", widget=forms.SelectDateWidget)
-    '''
--- app_becas/migrations/0003_typescholarship.py
+++ app_becas/migrations/0003_typescholarship.py
@@ -13,8 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='TypeScholarship',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=50, unique=True)),
+                ('name', models.CharField(max_length=50, unique=True, primary_key=True)),
             ],
         ),
     ]
