40ea9e5455cfb41eae751bf5d701627230581725
Juan Diego Lora <juan.lora1@u.icesi.edu.co>
2023-10-11 19:33:41 -0500

    fix: solve typeScholarship's id field

--- app_becas/migrations/0001_initial.py
+++ app_becas/migrations/0001_initial.py
@@ -64,7 +64,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='TypeScholarship',
             fields=[
-                ('name', models.CharField(max_length=50, unique=True)),
+                ('name', models.CharField(max_length=50, primary_key=True)),
             ],
         ),
         migrations.CreateModel(
