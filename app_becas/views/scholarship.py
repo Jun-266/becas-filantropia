89f91764b6cfa53c407da5c1a78653ace5977eb4
Juan Diego Lora <juan.lora1@u.icesi.edu.co>
2023-10-13 09:08:24 -0500

    fix: solve merging clonficts

--- app_becas/views/scholarship.py
+++ app_becas/views/scholarship.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-from typing import Any
 from django.shortcuts import redirect, render
-=======
-from django.shortcuts import render
->>>>>>> feat-40
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -17,11 +12,7 @@ class Scholarship(View):
     def get(self, request):
         scholarships = Scholarship_obj.objects.all()
         return render(request, 'scholarship.html', {
-<<<<<<< HEAD
-            'scholarships' : scholarships
-=======
             'scholarships': scholarships,
->>>>>>> feat-40
         })
 
     def search_by_name(request):
