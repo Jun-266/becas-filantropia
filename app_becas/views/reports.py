448e23e9a1a980c99e32cda988d3eec997b3d5b4
David Jun Kim <david.kim1@u.icesi.edu.co>
2023-10-13 22:39:40 -0500

    feat: Login requerido para los informes.

--- app_becas/views/reports.py
+++ app_becas/views/reports.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from app_becas.models import File
 from app_becas.forms.upload_file_form import UploadFileForm
 
 main_dir = '../templates/report_management/'
 
 
+@login_required
 def home(request):
     files = File.objects.all()
     return render(request, main_dir + 'home_reports.html', {
@@ -12,6 +14,7 @@ def home(request):
     })
 
 
+@login_required
 def upload_report(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
@@ -25,9 +28,11 @@ def upload_report(request):
     })
 
 
+@login_required
 def external_reports(request):
     return render(request, main_dir + 'external_reports.html')
 
 
+@login_required
 def generate_report(request):
     return render(request, main_dir + 'generate_report.html')
