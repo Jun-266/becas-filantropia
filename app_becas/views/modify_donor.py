56c3005c581fb55f82ca63e87d8e510c0950ad12
Jeison Lasprilla <1114238132@u.icesi.edu.co>
2023-10-16 00:23:36 -0500

    feat(modify_donor): donor linked to contact and scholarship

--- app_becas/views/modify_donor.py
+++ app_becas/views/modify_donor.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.views import View
-from app_becas.models import Donor
+from app_becas.models import Donor, Scholarship, Contact
 
 class Modify_donor(View):
     def get(self, request, auto_id):
@@ -9,9 +9,30 @@ class Modify_donor(View):
         return render(request, "modify_donor.html", {"donor": donor})
 
     def post(self, request, auto_id):
-        enterprise_name =request.POST['enterprise_name']
-        donor = Donor.objects.get(auto_id = auto_id)
-        donor.enterprise_name = enterprise_name
-        donor.save()
+        action = request.POST.get('action', None)
+        if action == 'asociar_beca':
+            return self.asociar_beca(request, auto_id)
+        elif action == 'asociar_contacto':
+            return self.asociar_contacto(request, auto_id)
+        else:
+            enterprise_name = request.POST['enterprise_name']
+            donor = Donor.objects.get(auto_id=auto_id)
+            donor.enterprise_name = enterprise_name
+            donor.save()
+            return redirect("manage_donor")
 
-        return redirect("manage_donor")
+    def asociar_beca(self, request, auto_id):
+        donor = Donor.objects.get(auto_id=auto_id)
+        scholarship_id = request.POST['donor_scholarship']
+        scholarship = Scholarship.objects.get(auto_id=scholarship_id)
+        donor.scholarships.add(scholarship)
+        donor.save()
+        return redirect('manage_donor')
+
+    def asociar_contacto(self, request, auto_id):
+        donor = Donor.objects.get(auto_id=auto_id)
+        contact_id = request.POST['donor_contact']
+        contact = Contact.objects.get(auto_id=contact_id)
+        donor.contacts.add(contact)
+        donor.save()
+        return redirect('manage_donor')
