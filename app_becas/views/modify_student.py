06589178ca1ed10cf2f63287647ad565523f6f19
Jeison Lasprilla <1114238132@u.icesi.edu.co>
2023-10-30 09:55:54 -0500

    feat(modify_student.py): add major and modify student with major

--- app_becas/views/modify_student.py
+++ app_becas/views/modify_student.py
@@ -5,8 +5,22 @@ from app_becas.models import Student, Scholarship, Major
 
 class Modify_student(View):
     def get(self, request, auto_id):
+
         student = Student.objects.get(auto_id=auto_id)
-        return render(request, "modify_student.html", {"student": student})
+        
+        try:
+            major = Major.objects.get(auto_id=student.major_id)
+        except Major.DoesNotExist:
+            major = Major.objects.create(
+                        name = "",
+                        description = ""
+                        )
+            major.save()
+
+        student.major = major
+        student.save()
+
+        return render(request, "modify_student.html", {"student": student, "major": major})
 
     def post(self, request, auto_id):
         action = request.POST.get('action', None)
@@ -16,21 +30,28 @@ class Modify_student(View):
             return self.asociar_major(request, auto_id)
         else:
             #enterprise_name = request.POST['enterprise_name']
-            #apellido
-            #Demas datos del estudiante
-            ##datos de major también para  actualizar
             #major = Major.objects.get(auto_id=major_id)
-            #major.auto_id
-            #major.name
             student = Student.objects.get(auto_id=auto_id)
-            #student.enterprise_name = enterprise_name
+            student.code = request.POST['code']
+            student.name = request.POST['name']
+            student.lastname = request.POST['lastname']
+            student.school = request.POST['school']
+            student.email = request.POST['email']
+            student.phone = request.POST['phone']
+            student.first_semester = request.POST['first_semester']
+            student.last_semester = request.POST['last_semester']
             student.save()
             return redirect("manage_student")
 
     def asociar_major(self, request, auto_id):
         student = Student.objects.get(auto_id=auto_id)
-        major_id = request.POST['major_id']
-        major = Major.objects.get(auto_id=major_id)
+
+        #Actualizar major
+        major = student.major
+        major.name = request.POST['major_name']
+        major.description = request.POST['major_description']
+        major.save()
+        #Link student-major
         student.major = major
         student.save()
         return redirect('manage_student')
