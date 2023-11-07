b417e14abc01e3e8d8945a6485147f3eaf41fbdf
Jeison Lasprilla <1114238132@u.icesi.edu.co>
2023-11-06 19:54:59 -0500

    feat(modify_student.py): update aplication status v1

--- app_becas/views/modify_student.py
+++ app_becas/views/modify_student.py
@@ -3,39 +3,67 @@ from django.shortcuts import redirect, render
 from django.views import View
 from app_becas.models import Student, Scholarship, Major, ScholarshipApplication, ApplicationStatus
 
+
 class Modify_student(View):
     def get(self, request, auto_id):
         student = Student.objects.get(auto_id=auto_id)
         majors = Major.objects.all()
-        return render(request, "modify_student.html", {"student": student, "majors": majors})
+        context = {
+            "student": student,
+            "majors": majors,
+        }
+        return render(request, "modify_student.html", context)
 
     def post(self, request, auto_id):
-        action = request.POST.get('action', None)
-        if action == 'asociar_beca':
+        action = request.POST.get("action", None)
+
+        if action == "asociar_beca":
             return self.asociar_beca(request, auto_id)
+        elif action == "update_status":
+            return self.update_status(request, auto_id)
         else:
             student = Student.objects.get(auto_id=auto_id)
-            student.code = request.POST['code']
-            student.name = request.POST['name']
-            student.lastname = request.POST['lastname']
-            student.school = request.POST['school']
-            student.email = request.POST['email']
-            student.phone = request.POST['phone']
-            student.first_semester = request.POST['first_semester']
-            student.last_semester = request.POST['last_semester']
-            student.major = Major.objects.get(auto_id=request.POST['major_select'])
+            student.code = request.POST.get("code")
+            student.name = request.POST.get("name")
+            student.lastname = request.POST.get("lastname")
+            student.school = request.POST.get("school")
+            student.email = request.POST.get("email")
+            student.phone = request.POST.get("phone")
+            student.first_semester = request.POST.get("first_semester")
+            student.last_semester = request.POST.get("last_semester")
+            student.major = Major.objects.get(auto_id=request.POST.get("major_select"))
             student.save()
             return redirect("manage_student")
-    
+
     def asociar_beca(self, request, auto_id):
         student = Student.objects.get(auto_id=auto_id)
-        scholarship_id = request.POST['student_scholarship']
+        scholarship_id = request.POST.get("student_scholarship")
         scholarship = Scholarship.objects.get(auto_id=scholarship_id)
-        
-        # Obten la instancia de ApplicationStatus con nombre "En proceso"
+
         application_status = ApplicationStatus.objects.get(type="En proceso")
-        
-        # Crea una ScholarshipApplication para el estudiante y la beca, y asigna la instancia de ApplicationStatus
-        ScholarshipApplication.objects.create(student=student, scholarship=scholarship, application_status=application_status)
-        
-        return redirect('manage_student')
+
+        ScholarshipApplication.objects.create(
+            student=student,
+            scholarship=scholarship,
+            application_status=application_status,
+        )
+
+        return HttpResponseRedirect(request.path)
+    
+    def update_status(self, request, auto_id):
+        student = Student.objects.get(auto_id=auto_id)
+
+        for application in student.scholarshipapplication_set.all():
+            application_id = application.id
+            status = request.POST.get(f"status_{application_id}")
+            application_status = ApplicationStatus.objects.get(type=status)
+            application.application_status = application_status
+            application.application_status.save()
+            application.save()
+
+        return HttpResponseRedirect(request.path)
+
+
+
+
+
