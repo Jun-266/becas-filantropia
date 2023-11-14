5d3d942be1e431462ca0d9261b35a56eae0071bf
David Jun Kim <david.kim1@u.icesi.edu.co>
2023-11-13 20:06:54 -0500

    refactor: Adding some fixes.

--- modulobecas/urls.py
+++ modulobecas/urls.py
@@ -10,7 +10,7 @@ from app_becas.views.calendar import Calendar
 from app_becas.views.calendar_add import Calendar_add
 from app_becas.views.calendar_show_info import Calendar_show_info
 from app_becas.views.manage_student import Manage_student
-from app_becas.views.manage_user import Manage_user
+from app_becas.views.manageuser import ManageUser
 from app_becas.views.manage_donor import Manage_donor
 from app_becas.views.manage_contact import Manage_contact
 from app_becas.views.scholarship import Scholarship
@@ -58,7 +58,7 @@ urlpatterns = [
     path('reports_external/', hr.external_reports, name='external_reports'),
     path('reports_generate/', hr.generate_report, name='generate_report'),
     
-    path('manage_user/', Manage_user.as_view(), name='manage_user'),
+    path('manage_user/', ManageUser.as_view(), name='manage_user'),
     path('manage_student/', Manage_student.as_view(), name='manage_student'),
     path('manage_user/delete_user/<str:auto_id>/', Delete_user.as_view(), name='delete_user'),
     path('manage_contact/', Manage_contact.as_view(), name='manage_contact'),
