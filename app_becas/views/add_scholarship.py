25d0bf4072e61719f81dbe940ec12b726b50481e
David Jun Kim <david.kim1@u.icesi.edu.co>
2023-11-16 22:14:17 -0500

    refactor: Fixing some on the GUI.

--- app_becas/views/add_scholarship.py
+++ app_becas/views/add_scholarship.py
@@ -20,13 +20,11 @@ class Add_scholarship(View):
             name = form.cleaned_data['name']
             description = form.cleaned_data['description']
             amount = form.cleaned_data['amount']
-            type_scholarship = form.cleaned_data['type_scholarship']
-            
+
             Scholarship.objects.create(
                 name=name,
                 description=description,
                 amount=amount,
-                type_scholarship=type_scholarship
             )
 
         return redirect('scholarship')
